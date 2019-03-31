00984db8e6b6827c1195912365a289582800f960
Kacper Szarkiewicz <SzarkiewiczMail@gmail.com>
2019-03-31 13:37:40 +0200

    Xeyes

---
+++ Javascript Projects/Eyes/script.js
@@ -0,0 +1,24 @@
+let irises = document.getElementsByClassName('Iris');
+let website = document.querySelector("html");
+
+website.addEventListener('mousedown', function(){
+    for(i=0;i<2;i++){
+        irises[i].style.backgroundColor = "red";
+    }
+});
+website.addEventListener('mouseup', function(){
+    for(i=0;i<2;i++){
+        irises[i].style.backgroundColor = "black";
+    }
+});
+
+document.onmousemove = function(){
+    let x = event.clientX * 100 /window.innerWidth + "%";
+    let y = event.clientY * 100 /window.innerWidth + "%";
+
+    for(i=0;i<2;i++){
+        irises[i].style.left = x;
+        irises[i].style.top = y;
+        irises[i].style.transform = `translate(-${x}, -${y})`;
+    }
+}
